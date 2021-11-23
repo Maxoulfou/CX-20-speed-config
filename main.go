9f3240f1c6f5c6e500b6eef3a07cc562d4408d63
Makussu <max.brochier1@gmail.com>
2021-11-23 19:37:37 +0100

    Fix bug Don't uncomment : - `logs.WriteLogs("fatal", "("+file+" : "+strconv.Itoa(line)+") TEST FATAL LOG")` - `logs.WriteLogs("", "fatal test")`
    It will broke code

--- main.go
+++ main.go
@@ -19,14 +19,16 @@ func main() {
 	logs.WriteLogs("warn", "("+file+" : "+strconv.Itoa(line)+") TEST WARN LOG")
 	logs.WriteLogs("info", "("+file+" : "+strconv.Itoa(line)+") TEST INFO LOG")
 	logs.WriteLogs("debug", "("+file+" : "+strconv.Itoa(line)+") TEST DEBUG LOG")
-	logs.WriteLogs("fatal", "("+file+" : "+strconv.Itoa(line)+") TEST FATAL LOG")
+	// Must be commented, this snippet broke code
+	// logs.WriteLogs("fatal", "("+file+" : "+strconv.Itoa(line)+") TEST FATAL LOG")
 
 	BarcoConfig, _ := configuration.LoadConfiguration("config.json")
 	config, _ := format.PrettyStruct(BarcoConfig)
 	logs.WriteLogs("info", "("+file+" : "+strconv.Itoa(line)+") Loaded configuration:\n"+config)
 	logs.WriteLogs("info", "--- End loaded configuration ---")
+	// Must be commented, this snippet broke code
 	// test fatal log when I cant set loglevel :
-	logs.WriteLogs("", "fatal test")
+	// logs.WriteLogs("", "fatal test")
 
 	os.Exit(0)
 }
