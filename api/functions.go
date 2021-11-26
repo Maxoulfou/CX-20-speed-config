b2b0e274548f22ffbfaf42ec6a5be5ab7eb4f8de
max.brochier1@gmail.com <hFEquTKmU9gr%/%>
2021-11-26 18:06:37 +0100

    Update functions.go - CheckIfBarcoCxApiIsReachable() To check that the api of the Barco cx-20 is reachable, I get the information from the station, if the code 200 is returned, then everything is ok, otherwise there is an error, the code break
    for MakeRequest func, I update APIKEY word, ApiKey -> ApiToken

--- api/functions.go
+++ api/functions.go
@@ -1,10 +1,14 @@
 package api
 
 import (
+	"crypto/tls"
 	"cx-20-api/configuration"
+	"cx-20-api/format"
+	"cx-20-api/logs"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strings"
-	"time"
 )
 
 // MakeRequest is the generic function to execute CX20API requests
@@ -24,7 +28,7 @@ func MakeRequest(path string, params string, method string) (response *http.Resp
 	}
 
 	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("APIKEY", cfg.ApiKey) // add CX-20 api key
+	req.Header.Add("APIKEY", cfg.ApiToken) // add CX-20 api key
 
 	response, responseError := client.Do(req)
 	if responseError != nil {
@@ -40,21 +44,57 @@ func MakeRequest(path string, params string, method string) (response *http.Resp
 	return response, error
 }
 
-func CheckIfBarcoCxApiIsReachable() {
+// CheckIfBarcoCxApiIsReachable check if barco api is reachable with getting device identity.
+func CheckIfBarcoCxApiIsReachable() bool {
 	cfg := configuration.GetEnv()
-	BarcoApiUrl := cfg.ApiUrl
-	URL := BarcoApiUrl + "/explorer/"
-	timeout := 500 * time.Second
 
-	client := http.Client{
-		Timeout: timeout,
-	}
+	// TODO : make insecure request trough http transport method only for knowing end-devices
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 
-	_, err := client.Get(URL)
+	logs.WriteLogs("info", "CheckIfBarcoCxApiIsReachable start")
+	url := cfg.ApiUrl + "/configuration/system/device-identity"
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
 
 	if err != nil {
-		panic(err.Error())
-
-		return
+		fmt.Println(err)
+		return false
 	}
+
+	Auth := "Basic " + cfg.ApiToken
+
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Authorization", Auth)
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	defer res.Body.Close()
+
+	status := res.Status
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+
+	FinalBody := string(body)
+	FinalBody, _ = format.PrettyString(FinalBody)
+
+	logs.WriteLogs("info", "api-CheckIfBarcoCxApiIsReachable : \n"+FinalBody)
+
+	switch status {
+	case "200 OK":
+		return true
+		break
+	default:
+		return false
+		break
+	}
+	return false
 }
