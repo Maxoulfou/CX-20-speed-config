0ed45c3ca98bdefaa605706f2e693d2c0e8734c0
max.brochier1@gmail.com <hFEquTKmU9gr%/%>
2021-11-29 16:33:45 +0100

    Update entity package Adding interface for Yml and Barco struct

--- entity/config.go
+++ entity/config.go
@@ -1,9 +1,50 @@
 package entity
 
-// Config is struct for yml config env and different state or choice
-// env : (debug,test,pre-prod,prod,alpha,beta)
-// log : (all-in-one, separated)
-type Config struct {
+import (
+	"encoding/json"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+)
+
+type ConfigIT interface {
+	GetConfig()
+}
+
+func (receiver *YmlConfig) GetConfig() *YmlConfig {
+	yamlFile, ErrReadYamlFile := ioutil.ReadFile("env-prod.yml")
+	if ErrReadYamlFile != nil {
+		log.Fatalf("YAML config.GetConfig ErrReadYamlFile: " + ErrReadYamlFile.Error())
+	}
+
+	ErrUnmarshalYamlFile := yaml.Unmarshal(yamlFile, receiver)
+	if ErrUnmarshalYamlFile != nil {
+		log.Fatalf("YAML config.GetConfig ErrUnmarshalYamlFile: " + ErrUnmarshalYamlFile.Error())
+	}
+
+	return receiver
+}
+
+type BarcoConfigIT interface {
+	GetConfig()
+}
+
+func (receiver *Barco) GetConfig() *Barco {
+	jsonFile, ErrReadJsonBarcoFile := ioutil.ReadFile("config.json")
+	if ErrReadJsonBarcoFile != nil {
+		log.Fatalf("JSON config.GetConfig ErrReadJsonBarcoFile: " + ErrReadJsonBarcoFile.Error())
+	}
+
+	ErrUnmarshalJsonBarcoFile := json.Unmarshal(jsonFile, receiver)
+	if ErrUnmarshalJsonBarcoFile != nil {
+		log.Fatalf("JSON config.GetConfig ErrUnmarshalJsonBarcoFile: " + ErrUnmarshalJsonBarcoFile.Error())
+	}
+
+	return receiver
+}
+
+// YmlConfig is struct for yml config env and different state or choice
+type YmlConfig struct {
 	Env         string `yaml:"env"`
 	Log         string `yaml:"log"`
 	ApiUrl      string `yaml:"api-url"`
@@ -12,10 +53,9 @@ type Config struct {
 	ApiToken    string `yaml:"api-token"`
 }
 
-// Barco is struct for json config. It will be used for configure your
-// CX-20 clickshare module
-// TODO : make IP struct
+// Barco is struct for json config. It will be used for configure your / CX-20 clickshare module
 type Barco struct {
+	// TODO : make ip struct
 	Network struct {
 		IP         string
 		SubnetMask string
