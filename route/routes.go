7b1f1b7d9af60e3ef450b6e6d9ffb1575ed399c6
Makussu <max.brochier1@gmail.com>
2021-12-02 21:36:17 +0100

    Factorization of code Make CONST system with Dictionary

--- route/routes.go
+++ route/routes.go
@@ -1,10 +1,32 @@
 package route
 
-var SystemStatus = "/configuration/system/status"                   // GET
-var Reboot = "/operations/reboot"                                   // POST
-var Personalization = "/configuration/personalization"              // PATCH
-var GetWallpaper = "/configuration/wallpapers"                      // GET
-var UpdateWallpaper = "/configuration/wallpapers/selected"          // PATCH
-var UpdateServiceAirPlay = "/configuration/features/airplay"        // PATCH
-var UpdateServiceGoogleCast = "/configuration/features/google-cast" // PATCH
-var UpdateHostName = "/configuration/system/network"                // PATCH
+import (
+	"cx-20-api/entity"
+	"fmt"
+)
+
+const (
+	SYSTEM_STATUS              = "SystemStatus"
+	REBOOT                     = "Reboot"
+	PERSONALIZATION            = "Personalization"
+	GET_WALLPAPER              = "GetWallpaper"
+	UPDATE_WALLPAPER           = "UpdateWallpaper"
+	UPDATE_SERVICE_AIR_PLAY    = "UpdateServiceAirPlay"
+	UPDATES_ERVICE_GOOGLE_CAST = "UpdateServiceGoogleCast"
+	UPDATE_HOSTNAME            = "UpdateHostName"
+)
+
+func RoutesDictionary() map[string]entity.Route {
+	RoutesDict := make(map[string]entity.Route)
+	RoutesDict["SystemStatus"] = entity.Route{Path: "/configuration/system/status", Method: "GET"}
+	RoutesDict["Reboot"] = entity.Route{Path: "/operations/reboot", Method: "POST"}
+	RoutesDict["Personalization"] = entity.Route{Path: "/configuration/personalization", Method: "PATCH"}
+	RoutesDict["GetWallpaper"] = entity.Route{Path: "/configuration/wallpapers", Method: "GET"}
+	RoutesDict["UpdateWallpaper"] = entity.Route{Path: "/configuration/wallpapers/selected", Method: "PATCH"}
+	RoutesDict["UpdateServiceAirPlay"] = entity.Route{Path: "/configuration/features/airplay", Method: "PATCH"}
+	RoutesDict["UpdateServiceGoogleCast"] = entity.Route{Path: "/configuration/features/google-cast", Method: "PATCH"}
+	RoutesDict["UpdateHostName"] = entity.Route{Path: "/configuration/system/network", Method: "PATCH"}
+	fmt.Printf("RoutesDict: %+v\n", RoutesDict)
+
+	return RoutesDict
+}
