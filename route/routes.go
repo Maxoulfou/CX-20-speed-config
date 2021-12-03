313d8cced8452652236e8128bb0b824cc7b6c117
Maxoulfou <hFEquTKmU9gr%/%>
2021-12-03 16:25:43 +0100

    Adding console output Adding map displaying as string

--- route/routes.go
+++ route/routes.go
@@ -1,7 +1,9 @@
 package route
 
 import (
+	"bytes"
 	"cx-20-api/entity"
+	"cx-20-api/logs"
 	"fmt"
 )
 
@@ -9,24 +11,44 @@ const (
 	SYSTEM_STATUS              = "SystemStatus"
 	REBOOT                     = "Reboot"
 	PERSONALIZATION            = "Personalization"
+	UPLOAD_WALLPAPER           = "UploadWallpaper"
 	GET_WALLPAPER              = "GetWallpaper"
 	UPDATE_WALLPAPER           = "UpdateWallpaper"
 	UPDATE_SERVICE_AIR_PLAY    = "UpdateServiceAirPlay"
 	UPDATES_ERVICE_GOOGLE_CAST = "UpdateServiceGoogleCast"
+	UPDATE_SSID                = "UpdateSsid"
 	UPDATE_HOSTNAME            = "UpdateHostName"
 )
 
-func RoutesDictionary() map[string]entity.Route {
+func RoutesDictionary(console bool) map[string]entity.Route {
 	RoutesDict := make(map[string]entity.Route)
 	RoutesDict["SystemStatus"] = entity.Route{Path: "/configuration/system/status", Method: "GET"}
 	RoutesDict["Reboot"] = entity.Route{Path: "/operations/reboot", Method: "POST"}
 	RoutesDict["Personalization"] = entity.Route{Path: "/configuration/personalization", Method: "PATCH"}
+	RoutesDict["UploadWallpaper"] = entity.Route{Path: "/configuration/wallpapers", Method: "POST"}
 	RoutesDict["GetWallpaper"] = entity.Route{Path: "/configuration/wallpapers", Method: "GET"}
 	RoutesDict["UpdateWallpaper"] = entity.Route{Path: "/configuration/wallpapers/selected", Method: "PATCH"}
 	RoutesDict["UpdateServiceAirPlay"] = entity.Route{Path: "/configuration/features/airplay", Method: "PATCH"}
 	RoutesDict["UpdateServiceGoogleCast"] = entity.Route{Path: "/configuration/features/google-cast", Method: "PATCH"}
+	RoutesDict["UpdateSsid"] = entity.Route{Path: "/configuration/system/network/wireless/1", Method: "PATCH"}
 	RoutesDict["UpdateHostName"] = entity.Route{Path: "/configuration/system/network", Method: "PATCH"}
-	fmt.Printf("RoutesDict: %+v\n", RoutesDict)
+
+	if console {
+		// keyValuePair will get all routes in dict : [key] = [value]
+		keyValuePair := createKeyValuePairs(RoutesDict)
+		logs.WriteLogs("info", "RoutesDict : "+keyValuePair, true)
+	}
 
 	return RoutesDict
+}
+
+func createKeyValuePairs(m map[string]entity.Route) string {
+	b := new(bytes.Buffer)
+	fmt.Fprintf(b, "\n")
+	for key, value := range m {
+		fmt.Fprintf(b, "\t˫")
+		fmt.Fprintf(b, "%s=\"%s\"\n", key, value)
+	}
+	fmt.Fprintf(b, "------------------------------------------------------------------------------")
+	return b.String()
 }
